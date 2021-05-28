57c21ccab805111d83d038c4d9fa5f1dd589fed9
sathya177 <47978849+sathya177@users.noreply.github.com>
2021-05-28 15:46:55 +0530

    Update HelloController.java

--- gke/src/main/java/hello/HelloController.java
+++ gke/src/main/java/hello/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!, This is for Testing";
+        return "Greetings from Spring Boot!";
     }
     
 }
